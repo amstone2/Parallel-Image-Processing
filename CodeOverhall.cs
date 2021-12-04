834f6cecfce50028cae399c3a9997278be2f26bb
Alex Stone <alexstone@TPAB.local>
2021-12-04 14:05:40 -0500

    backup

--- CodeOverhall.cs
+++ CodeOverhall.cs
@@ -24,19 +24,37 @@ using System.Windows.Forms;
 
 /************************************************************/
 
-    public class ThreadData {
+    public class ThreadDataForCompression {
         public Rectangle rec;
         public readonly Graphics g;
         public readonly Bitmap bmp;
         public int compressionValue;
         public readonly CountdownEvent cntEvent;
 
-        public ThreadData(Rectangle r, ref Graphics g, in Bitmap bitmap, int cv, ref CountdownEvent ce) {
+        public ThreadDataForCompression(Rectangle r, ref Graphics g, in Bitmap bitmap, int cv, ref CountdownEvent ce) {
           this.rec = r;
           this.g = g;
           this.bmp = bitmap;
           this.compressionValue = cv;
           this.cntEvent = ce;
+        }
+    }
+
+
+        public class ThreadDataForModification {
+        public Rectangle rec;
+        public readonly Bitmap bmp;
+        public Bitmap newBmp;
+
+        public int modificationValue;
+        public readonly CountdownEvent cntEvent;
+
+        public ThreadDataForModification(Rectangle r, ref Bitmap bitmap, ref Bitmap newBmp, int modificationValue, ref CountdownEvent cntEvent) {
+          this.rec = r;
+          this.bmp = bitmap;
+          this.newBmp = newBmp;
+          this.modificationValue = modificationValue;
+          this.cntEvent = cntEvent;
         }
     }
 /************************************************************/
@@ -51,7 +69,12 @@ class CodeOverhall
         {
             int threads = Int32.Parse(args[0]);
             String filename = args[1];
+
             String newFilename = args[2];
+            int index = newFilename.IndexOf(".");
+            String serialNewFileName = newFilename.Substring(0, index);
+            serialNewFileName += "Serial.jpg";
+
             String mode = args[3];
 
             // For compression
@@ -59,25 +82,20 @@ class CodeOverhall
             {
                 int compressionValue = Int32.Parse(args[4]);
 
-                compressImageParallel (
-                    threads,
-                    filename,
-                    newFilename,
-                    compressionValue
-                );
+                compressImageParallel (threads, filename, newFilename, compressionValue);
+                compressImageSerial (filename, serialNewFileName, compressionValue);
 
-                int index = newFilename.IndexOf(".");
-                String serialNewFileName = newFilename.Substring(0, index);
-                serialNewFileName += "Serial.jpg";
-
-                compressImageSerial (
-                    filename,
-                    serialNewFileName,
-                    compressionValue
-                );
-
-            } 
-
+            }
+            else if(mode == "baw")
+            {
+              blackAndWhiteImageInParallel(threads, filename, newFilename);
+              blackAndWhiteImageSerial(filename, serialNewFileName);
+            }
+            else if(mode == "flip")
+            {
+              flipInParallel(threads, filename, newFilename);
+              flipImageSerial(filename, serialNewFileName);
+            }
         else
         {
             // Help message.
@@ -87,7 +105,8 @@ class CodeOverhall
             Console.WriteLine("Arg2 = new fileanme");
             Console.WriteLine("Arg3 = Mode (compress)");
         }
-      }
+      
+      } 
     }
 
     /************************************************************/
@@ -133,7 +152,7 @@ class CodeOverhall
         // Go through all the rectangles, compressing and drawing them back on the bitmap.
         foreach (var rec in partitionedRectangles)
         {
-            var data = new ThreadData(rec, ref g, in bmp, compressionValue, ref cntEvent);
+            var data = new ThreadDataForCompression(rec, ref g, in bmp, compressionValue, ref cntEvent);
             ThreadPool.QueueUserWorkItem(s => compressRectangleAndDraw(s), data);
         }
         
@@ -142,10 +161,10 @@ class CodeOverhall
         cntEvent.Wait();
 
         // Create and save the final bitmap.
-        finalBitmap.Save(newFilename, System.Drawing.Imaging.ImageFormat.Jpeg);
         watch.Stop();
         long parTime = watch.ElapsedMilliseconds;
         Console.WriteLine("New Parallel time Time time for compression: " + parTime + " ms\n\n");
+        finalBitmap.Save(newFilename, System.Drawing.Imaging.ImageFormat.Jpeg);
     }
 
 
@@ -239,7 +258,7 @@ class CodeOverhall
     /************************************************************/
     public static void compressRectangleAndDraw(object d)
     {
-        ThreadData data = (ThreadData)d;
+        ThreadDataForCompression data = (ThreadDataForCompression)d;
         var rec = data.rec;
         var g = data.g;
         var bmp = data.bmp;
@@ -316,12 +335,208 @@ class CodeOverhall
         Bitmap bmp = new Bitmap(filename);
         MemoryStream ms = CompressImage (bmp, compressionValue);
         var compressedImage = Image.FromStream(ms);
-              compressedImage.Save(newFilename);
+
+        // compressedImage.Save(newFilename);
 
         watch.Stop();
         long serialTime = watch.ElapsedMilliseconds;
         Console.WriteLine("Serial Time time for compression: " + serialTime + " ms\n");
     }
     /************************************************************/
+
+
+      public static void blackAndWhiteImageInParallel(
+        int threads,
+        string filename,
+        String newFilename
+    )
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        // New bitmap of the input image.
+        Bitmap bmp = new Bitmap(filename);
+        Bitmap newBmp = new Bitmap(0,0);
+
+        // threads passed to partitioning algorithm.
+        int numOfColumns = threads / 2;
+
+        // Get the rectangles needed to modify the image in.
+        Rectangle[] partitionedRectangles = new Rectangle[threads];
+        getRectangles(ref bmp,
+        threads,
+        ref partitionedRectangles);
+
+        // Count down event so we can see how many threads have been completed.
+        CountdownEvent cntEvent = new CountdownEvent(threads);
+
+        int modificationValue = 0;
+        // Go through all the elements in the partioned list and apply the filter.
+        foreach (var rec in partitionedRectangles)
+        {
+            var data = new ThreadDataForModification(rec, ref bmp, ref newBmp, modificationValue, ref cntEvent);
+            ThreadPool.QueueUserWorkItem(s => setPixelBlackAndWhite(s), data);
+        }
+
+        // Wiat for all threads to finish.
+        cntEvent.Wait();
+
+        watch.Stop();
+        long parallelTime = watch.ElapsedMilliseconds;
+        Console.WriteLine("Serial Time time for baw: " + parallelTime + " ms\n");
+        // Save the bitmap to the new file name.
+        bmp.Save (newFilename);
+    }
+
+    public static void blackAndWhiteImageSerial(String filename, String newFilename)
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+
+        // New bitmap of the input image.
+        Bitmap bmp = new Bitmap(filename);
+        Bitmap newBmp = new Bitmap(0,0);
+
+        Rectangle rec = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        int modificationValue = 0;
+        CountdownEvent cntEvent = new CountdownEvent(1);
+
+        var data = new ThreadDataForModification(rec, ref bmp, ref newBmp, modificationValue, ref cntEvent);
+
+        setPixelBlackAndWhite(data);
+
+        watch.Stop();
+        long parallelTime = watch.ElapsedMilliseconds;
+        Console.WriteLine("Serial Time time for baw: " + parallelTime + " ms\n");
+        // bmp.Save (newFilename);
+    }
+
+
+    public static void setPixelBlackAndWhite(object d)
+    {
+        ThreadDataForModification data = (ThreadDataForModification)d;
+        var rec = data.rec;
+        var bmp = data.bmp;
+        var modificationValue = data.modificationValue;
+        var cntEvent = data.cntEvent;
+
+        int iStart = rec.X;
+        int iEnd = rec.Width + rec.X;
+        int jStart = rec.Y;
+        int jEnd = rec.Height + rec.Y;
+        // Go through the part of the image and apply the grey image.
+        for (int i = iStart; i < iEnd; ++i)
+        {
+            for (int j = jStart; j < jEnd; ++j)
+            {
+                Color c = bmp.GetPixel(i, j);
+
+                //Apply conversion equation
+                byte gray = (byte)(.21 * c.R + .71 * c.G + .071 * c.B);
+
+                //Set the color of this pixel
+                bmp.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+            }
+        }
+        cntEvent.Signal();
+    }
+
+  public static void flipInParallel(
+        int threads,
+        string filename,
+        String newFilename
+    )
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        // New bitmap of the input image.
+        Bitmap bmp = new Bitmap(filename);
+        Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+
+        // threads passed to partitioning algorithm.
+        int numOfColumns = threads / 2;
+
+        // Get the rectangles needed to modify the image in.
+        Rectangle[] partitionedRectangles = new Rectangle[threads];
+        getRectangles(ref bmp,
+        threads,
+        ref partitionedRectangles);
+
+        // Count down event so we can see how many threads have been completed.
+        CountdownEvent cntEvent = new CountdownEvent(threads);
+
+        int modificationValue = 0;
+        // Go through all the elements in the partioned list and apply the filter.
+        foreach (var rec in partitionedRectangles)
+        {
+            var data = new ThreadDataForModification(rec, ref bmp, ref newBmp, modificationValue, ref cntEvent);
+            ThreadPool.QueueUserWorkItem(s => flip(s), data);
+        }
+
+        // Wiat for all threads to finish.
+        cntEvent.Wait();
+
+        watch.Stop();
+        long parallelTime = watch.ElapsedMilliseconds;
+        Console.WriteLine("Parallel time for flip: " + parallelTime + " ms\n");
+        // Save the bitmap to the new file name.
+        newBmp.Save (newFilename);
+    }
+  
+  
+    public static void flipImageSerial(String filename, String newFilename)
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+
+        // New bitmap of the input image.
+        Bitmap bmp = new Bitmap(filename);
+        Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+        Rectangle rec = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        int modificationValue = 0;
+        CountdownEvent cntEvent = new CountdownEvent(1);
+
+        var data = new ThreadDataForModification(rec, ref bmp, ref newBmp, modificationValue, ref cntEvent);
+
+        flip(data);
+
+        watch.Stop();
+        long parallelTime = watch.ElapsedMilliseconds;
+        Console.WriteLine("Serial Time time for flip: " + parallelTime + " ms\n");
+        newBmp.Save (newFilename);
+    }
+
+    public static void flip(Object d)
+    {
+        ThreadDataForModification data = (ThreadDataForModification)d;
+        var rec = data.rec;
+        var bmp = data.bmp;
+        var newBmp = data.newBmp;
+        var modificationValue = data.modificationValue;
+        var cntEvent = data.cntEvent;
+
+        int iStart = rec.X;
+        int iEnd = rec.Width + rec.X;
+        int jStart = rec.Y;
+        int jEnd = rec.Height + rec.Y;
+
+
+        var w = newBmp.Width - 1;
+        for (int i = iStart; i < iEnd; ++i)
+        {
+            var h = newBmp.Height - 1;
+            for (int j = jStart; j < jEnd; ++j)
+            {
+                Color c = bmp.GetPixel(i, j);
+                newBmp.SetPixel (w, h, c);
+                h--;
+            }
+            w--;
+        }
+        cntEvent.Signal();
+    }
+
+    
 }
-    /************************************************************/
+
+
+/************************************************************/
